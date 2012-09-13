b873be13b93f33b9006a3861759c151837158dc5
Morten <morten.rasch@coop.dk>
2012-09-13 11:18:15 +0200

    Pushing new changes

---
+++ source/app.specs/ViewTheDepartmentsInADepartment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using app.web.app.catalogbrowsing;
+using app.web.core;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+    [Subject(typeof(ViewTheDepartmentsInADepartment))]
+    public class ViewTheDepartmentsInADepartmentSpec
+    {
+        public abstract class concern : Observes<IImplementAFeature, ViewTheDepartmentsInADepartment>
+        {
+        }
+
+
+        public class when_run : concern
+        {
+            Establish c = () =>
+            {
+                department_repository = depends.on<IFindDepartments>();
+                display_engine = depends.on<IDisplayInformation>();
+                request = fake.an<IEncapsulateRequestDetails>();
+                the_departments_in_a_given_department = new List<DepartmentItem> { new DepartmentItem() };
+                DepartmentItem parent_department = new DepartmentItem();
+                department_repository.setup(x => x.get_departments_in_a_given_department(parent_department)).Return(the_departments_in_a_given_department);
+            };
+
+
+            Because b = () =>
+              sut.process(request);
+
+            It should_display_the_departmenst_in_a_given_department = () =>
+              display_engine.received(x => x.display(the_departments_in_a_given_department));
+
+
+
+            static IEncapsulateRequestDetails request;
+            static IDisplayInformation display_engine;
+            private static IEnumerable<DepartmentItem> the_departments_in_a_given_department;
+            static IFindDepartments department_repository;
+
+        }
+    }
+
+    public class ViewTheDepartmentsInADepartment : IImplementAFeature
+    {
+        public void process(IEncapsulateRequestDetails request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- source/app/web/app/catalogbrowsing/IFindDepartments.cs
+++ source/app/web/app/catalogbrowsing/IFindDepartments.cs
@@ -5,5 +5,6 @@ namespace app.web.app.catalogbrowsing
   public interface IFindDepartments
   {
     IEnumerable<DepartmentItem> get_main_departments();
+    IEnumerable<DepartmentItem> get_departments_in_a_given_department(DepartmentItem parent_department);
   }
 }
