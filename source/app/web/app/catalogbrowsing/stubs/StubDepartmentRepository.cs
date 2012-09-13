f427ab8ba56ab06ace0e32444a6534ec612522c2
Morten <morten.rasch@coop.dk>
2012-09-13 11:24:53 +0200

    Pushing new changes

--- source/app/web/app/catalogbrowsing/stubs/StubDepartmentRepository.cs
+++ source/app/web/app/catalogbrowsing/stubs/StubDepartmentRepository.cs
@@ -9,5 +9,10 @@ namespace app.web.app.catalogbrowsing.stubs
     {
       return Enumerable.Range(1, 100).Select(x => new DepartmentItem{name = x.ToString("Department 0")});
     }
+
+      public IEnumerable<DepartmentItem> get_departments_in_a_given_department(DepartmentItem parent_department)
+      {
+          throw new System.NotImplementedException();
+      }
   }
 }
